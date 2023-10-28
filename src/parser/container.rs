b70780888d56cc0e0bfb8ef5667e8350b78e7985
Don Han <plestoon@gmail.com>
2023-10-28 13:29:49 +0800

    chore

--- src/parser/container.rs
+++ src/parser/container.rs
@@ -48,11 +48,11 @@ mod tests {
         let doc = r###"
 <?xml version="1.0" encoding="utf-8"?>
 <container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">
-	<rootfiles>
-		<rootfile full-path="epub/content.opf" media-type="application/oebps-package+xml"/>
-		<rootfile full-path="EPUB/content.opf" media-type="application/oebps-package+xml"/>
-		<rootfile />
-	</rootfiles>
+    <rootfiles>
+        <rootfile full-path="epub/content.opf" media-type="application/oebps-package+xml"/>
+        <rootfile full-path="EPUB/content.opf" media-type="application/oebps-package+xml"/>
+        <rootfile />
+    </rootfiles>
 </container>
         "###
         .trim();
